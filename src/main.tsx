71032549d0e7d8f676ed7bc0e6fbdbcaf3490215
Truong Nguyen <truong.nguyen201@gmail.com>
2026-02-10 23:19:42 -0500

    Add color palette

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/reset.css"
-import "./token.css";
+import "./styles/token.css";
 import "./index.css";
 import App from "./App.tsx";
 
