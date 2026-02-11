c1c0a71fda6ccb7a4cf998f5489229abe524651c
Truong Nguyen <truong.nguyen201@gmail.com>
2026-02-10 21:40:42 -0500

    Clean up and update CSS

--- src/sections/hero/HeroSection.tsx
+++ src/sections/hero/HeroSection.tsx
@@ -1,6 +1,7 @@
 import Button from "../../components/buttons/CTAbutton";
 import Badge from "../../components/badge/Badge";
 import { Heart, BookOpen, Sparkles, Flower2 } from "lucide-react";
+import "./hero.style.css"
 
 const HeroSection = () => {
     return (
@@ -8,9 +9,8 @@ const HeroSection = () => {
             <div className="hero-container">
                <Badge icon={Heart}>Professional Mental Health Support</Badge>
 
-                <h1 className="hero-title">Your Journey to
-                    <span className="text-primary">Wellness</span> Starts Here</h1>
-                <p className="hero-text">Compassionate, evidence-based therapy tailored to your unique needs. Take the first step towards a healthier, happier you.</p>
+                <h1 className="hero-title">Your Journey to <span className="text-primary">Wellness</span> Starts Here</h1>
+                <p className="hero-text">Curious about therapy but not sure where to start? Explore what therapy is, take a free self-assessment, and discover resources to support your well-being.</p>
 
                 <Button type="button" variant="primary" size="lg" className="mt-10">Take the Free Assessment</Button>
             </div>
