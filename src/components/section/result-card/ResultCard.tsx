43e75c082484489ea94e3cafd6594cc24171a883
Truong Nguyen <truong.nguyen201@gmail.com>
2026-02-23 21:06:56 -0500

    Update Pill styling and dsplay in smaller screen

--- src/components/section/result-card/ResultCard.tsx
+++ src/components/section/result-card/ResultCard.tsx
@@ -3,21 +3,21 @@ import StatusPill from "@/components/UI/status-pill/StatusPill";
 import type { LucideIcon } from "lucide-react";
 import type { StatusLevel } from "@/components/UI/status-pill/StatusPill";
 
-
 type ResultCardProps = {
   icon: LucideIcon;
   label: string;
   description: string;
   status: StatusLevel;
+  iconClass?: string
 };
 
-const ResultCard = ({ icon, label, description, status } : ResultCardProps) => (
+const ResultCard = ({ icon, label, description, status, iconClass }: ResultCardProps) => (
   <div className="p-5 bg-[hsl(var(--background))] shadow-sm rounded-lg">
-    <div className="flex justify-between items-center">
+    <div className="flex flex-col gap-6 justify-between items-center sm:flex-row">
       <div className="flex items-center gap-2">
         <IconBadge
           icon={icon}
-          iconClassName={`w-6 h-6`}
+          iconClassName={`w-6 h-6 ${iconClass}`}
           wrapperClassName="w-12 h-12 rounded-full"
         />
         <h4 className="text-[1.12rem] text-[hsl(var(--green-1))] font-medium">
@@ -25,10 +25,12 @@ const ResultCard = ({ icon, label, description, status } : ResultCardProps) => (
         </h4>
       </div>
 
-{/*  <StatusPill status={calculateStatus}/> */}
-      <StatusPill status={status}/>
+      {/*  <StatusPill status={calculateStatus}/> */}
+      <StatusPill status={status} />
     </div>
-    <div className="text-[hsl(var(--green-1))] text-[1rem] mt-5">{description}</div>
+    <div className="text-[hsl(var(--green-1))] text-[1rem] mt-5">
+      {description}
+    </div>
   </div>
 );
 export default ResultCard;
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -6,6 +6,7 @@ export type Category = {
   icon: LucideIcon;
   label: string;
   description: string;
+  iconClass?: string
 };
 
 export const categories: Category[] = [
@@ -13,48 +14,56 @@ export const categories: Category[] = [
     id: "emotional-regulation",
     icon: Heart,
     label: "Emotional Regulation",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+    iconClass: "text-[hsl(var(--pink))]"
   },
   {
     id: "sleep-health",
     icon: Waves,
     label: "Sleep Health",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+    iconClass: "text-[hsl(var(--blue))]"
   },
   {
     id: "anxiety",
     icon: Wind,
     label: "Anxiety",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+    iconClass: "text-[hsl(var(--primary))]"
   },
   {
     id: "motivation-interest",
     icon: Sun,
     label: "Motivation & Interest",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+     iconClass: "text-[hsl(var(--accent))]"
   },
   {
     id: "stress-coping",
     icon: TreeDeciduous,
     label: "Stress Coping",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+     iconClass: "text-[hsl(var(--primary))]"
   },
   {
     id: "relationships",
     icon: HeartHandshake,
     label: "Relationships",
     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+     iconClass: "text-[hsl(var(--coral))]"
   },
   {
     id: "trauma-impact",
     icon: ShieldAlert,
     label: "Trauma Impact",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+     iconClass: "text-[hsl(var(--blue))]"
   },
   {
     id: "self-image",
     icon: Flower2,
     label: "Self Image",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?"
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem dolores quia officiis saepe minus error aliquid recusandae sed, illum ea dolor quisquam adipisci ut nesciunt dolorem modi! Aspernatur, rerum?",
+    iconClass: "text-[hsl(var(--pink))]"
   },
 ];
