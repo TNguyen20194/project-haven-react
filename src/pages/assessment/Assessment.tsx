248fda8f54d53c78a8dc428e3d148ab9690030d8
Truong Nguyen <truong.nguyen201@gmail.com>
2026-03-06 02:20:08 -0500

    Install dependencies

--- src/pages/assessment/Assessment.tsx
+++ src/pages/assessment/Assessment.tsx
@@ -1,8 +1,34 @@
 import Questionnaire from "./sections/Questionnair";
+import IconBadge from "@/components/UI/feature-icon/IconBadge";
+import { ClipboardCheck } from "lucide-react";
 
 const Assessment = () => {
     return (
-            <Questionnaire />
+        <section className="relative z-10 px-6 py-10 overflow-hidden">
+      {/* Background Layer */}
+      {/* <div
+        className="absolute rounded-full bottom-10 right-10 w-[350px] h-[350px] overflow-hidden bg-no-repeat bg-center bg-cover opacity-90 pointer-events-none"
+        style={{ backgroundImage: `url(${flower3})` }}
+      /> */}
+
+      <div className="relative z-10 max-w-[56rem] mx-auto ">
+        <div className="flex items-center gap-3 mb-5">
+          <IconBadge
+            icon={ClipboardCheck}
+            wrapperClassName="rounded-full w-12 h-12"
+            iconClassName="text-[hsl(var(--primary))] h-7 w-7"
+          />
+          <h3 className="sub-heading">Explore Your Mental Wellness</h3>
+        </div>
+        <p className="p-text">
+          Answer a few questions to learn more about your mental wellness. This
+          is completely anonymous and free.
+        </p>
+      </div>
+
+        <Questionnaire />
+    </section>
+            
     )
 };
 
--- src/pages/assessment/sections/Questionnair.tsx
+++ src/pages/assessment/sections/Questionnair.tsx
@@ -1,41 +1,10 @@
-import IconBadge from "@/components/UI/feature-icon/IconBadge";
-import { ClipboardCheck } from "lucide-react";
-import flower3 from "@/assets/decorative/therapy-flower-3.jpg";
+import { Progress } from "@/components/UI/progress";
+import { useForm } from "@tanstack/react-form";
+import { Toaster } from "@/components/UI/sonner";
 
 const Questionnaire = () => {
   return (
-    <section className="relative z-10 px-6 py-10 overflow-hidden">
-      {/* Background Layer */}
-      {/* <div
-        className="absolute rounded-full bottom-10 right-10 w-[350px] h-[350px] overflow-hidden bg-no-repeat bg-center bg-cover opacity-90 pointer-events-none"
-        style={{ backgroundImage: `url(${flower3})` }}
-      /> */}
-
-      <div className="relative z-10 max-w-[56rem] mx-auto ">
-        <div className="flex items-center gap-3 mb-5">
-          <IconBadge
-            icon={ClipboardCheck}
-            wrapperClassName="rounded-full w-12 h-12"
-            iconClassName="text-[hsl(var(--primary))] h-7 w-7"
-          />
-          <h3 className="sub-heading">Explore Your Mental Wellness</h3>
-        </div>
-        <p className="p-text">
-          Answer a few questions to learn more about your mental wellness. This
-          is completely anonymous and free.
-        </p>
-      </div>
-
-        {/* Questionnaire */}
-
-      {/* <div className="absolute bottom-10 right-10 aspect-square w-full max-w-[300px]">
-        <img
-          src={flower3}
-          alt="Explore Your Mental Wellness"
-          className="w-full h-full rounded-full object-cover"
-        />
-      </div> */}
-    </section>
+    <Progress value={33} />
   );
 };
 
