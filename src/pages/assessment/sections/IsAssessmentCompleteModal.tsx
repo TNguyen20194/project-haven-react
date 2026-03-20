73835536673a660e77fef0324d143393cf16c5ec
Truong Nguyen <truong.nguyen201@gmail.com>
2026-03-20 15:47:18 -0400

    Add close button to modal

--- src/pages/assessment/sections/IsAssessmentCompleteModal.tsx
+++ src/pages/assessment/sections/IsAssessmentCompleteModal.tsx
@@ -5,8 +5,10 @@ import {
   AlertDialogDescription,
   AlertDialogHeader,
   AlertDialogFooter,
+  AlertDialogCancel,
 } from "@/components/UI/alert-dialog";
 import Button from "@/components/UI/buttons/CTAbutton";
+import { X } from "lucide-react";
 
 type AlertDialogProps = {
   open: boolean;
@@ -24,6 +26,15 @@ const IsAssessmentCompleteModal = ({
   return (
     <AlertDialog open={open} onOpenChange={onOpenChange}>
       <AlertDialogContent className="sm:max-w-[480px] rounded-2xl border border-[#dcded7] bg-[hsl(var(--white))]">
+        <AlertDialogCancel asChild>
+          <button
+            className="absolute right-4 top-4 flex items-center justify-center w-8! h-8! rounded-full p-0! text-[hsl(var(--primary))]/70 hover:text-[hsl(var(--primary))]! bg-[hsl(var(--white))]! transition shadow-sm bg-white"
+            aria-label="Close modal"
+          >
+            <X className="h-[18px]! w-[18px]!" />
+          </button>
+        </AlertDialogCancel>
+
         <AlertDialogHeader>
           <AlertDialogTitle className="text-[1.5rem] font-semibold text-[hsl(var(--green-1))]">
             You've already completed this assessment
@@ -41,18 +52,16 @@ const IsAssessmentCompleteModal = ({
         </p>
 
         <AlertDialogFooter className="mt-4 flex flex-col gap-3 sm:flex-row sm:justify-end">
-          <Button 
-          variant="secondary" 
-          onClick={onRetake} 
-          type="button" 
-          className="hover:bg-[hsl(var(--primary)/0.3)]!">
+          <Button
+            variant="secondary"
+            onClick={onRetake}
+            type="button"
+            className="hover:bg-[hsl(var(--primary)/0.3)]!"
+          >
             Retake Assessment
           </Button>
 
-          <Button 
-          variant="primary" 
-          onClick={onViewResult} 
-          type="button">
+          <Button variant="primary" onClick={onViewResult} type="button">
             View Results
           </Button>
         </AlertDialogFooter>
