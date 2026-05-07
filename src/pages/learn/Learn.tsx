e4f0b88af9d07b59afea0601a73b71ce004b31a8
Truong Nguyen <truong.nguyen201@gmail.com>
2026-05-07 19:16:19 -0400

    Add MythsVsFacts section

--- src/pages/learn/Learn.tsx
+++ src/pages/learn/Learn.tsx
@@ -2,7 +2,7 @@ import UnderstandingTherapy from "./sections/UnderstandingTherapy";
 import WhatToExpect from "./sections/WhattoExpect";
 import TypesOfTherapy from "./sections/TypesofTherapy";
 import HowTherapyMightHelp from "./sections/HowTherapyMightHelp";
-import MythSection from "./sections/MythsVsFacts";
+import MythsVsFacts from "./sections/MythsVsFacts";
 import CTABannerSection from "../home/sections/cta-banner/CTABannerSection";
 
 const Learn = () => {
@@ -13,7 +13,7 @@ const Learn = () => {
         <WhatToExpect />
         <HowTherapyMightHelp />
         <TypesOfTherapy />
-        <MythSection />
+        <MythsVsFacts />
         <CTABannerSection
           title="Ready to Explore Further?"
           description="Take our anonymous self-assessment to learn more about your well-being, or connect with a professional when you're ready."
