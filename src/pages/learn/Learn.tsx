c9fe549d907284cdf8b07707565aa498e0d153ea
Truong Nguyen <truong.nguyen201@gmail.com>
2026-05-06 20:28:10 -0400

    Update section name

--- src/pages/learn/Learn.tsx
+++ src/pages/learn/Learn.tsx
@@ -2,7 +2,7 @@ import UnderstandingTherapy from "./sections/UnderstandingTherapy";
 import WhatToExpect from "./sections/WhattoExpect";
 import TypesOfTherapy from "./sections/TypesofTherapy";
 import HowTherapyMightHelp from "./sections/HowTherapyMightHelp";
-import MythsVsFacts from "./sections/MythsVsFacts";
+import MythSection from "./sections/MythsVsFacts";
 import CTABannerSection from "../home/sections/cta-banner/CTABannerSection";
 
 const Learn = () => {
@@ -13,7 +13,7 @@ const Learn = () => {
         <WhatToExpect />
         <HowTherapyMightHelp />
         <TypesOfTherapy />
-        <MythsVsFacts />
+        <MythSection />
         <CTABannerSection
           title="Ready to Explore Further?"
           description="Take our anonymous self-assessment to learn more about your well-being, or connect with a professional when you're ready."
